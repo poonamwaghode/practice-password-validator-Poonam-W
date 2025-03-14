9017e5e641d54b07c0af165ae65aa991bf1fd246
PoonamW <poonamwaghode@gmail.com>
2025-03-13 23:21:14 -0500

    Initial commit: StudentScores

---
+++ StudentScores.js
@@ -0,0 +1,25 @@
+// Challenge 1: Student Scores
+// Create the allScores array with three inner arrays
+let allScores = [
+    [85, 90, 78], // Scores for Student 1
+    [92, 88, 84], // Scores for Student 2
+    [76, 81, 89]  // Scores for Student 3
+];
+console.log('Student Scores:', allScores);
+
+// Challenge 2: Build a Seating Grid
+// Create the theaterSeating 2D array
+let theaterSeating = [
+    ["A1", "A2", "A3", "A4"], // Row 1
+    ["B1", "B2", "B3", "B4"], // Row 2
+    ["C1", "C2", "C3", "C4"]  // Row 3
+];
+console.log('Theater Seating:', theaterSeating);
+
+// Challenge 3: Form a Multi-Dimensional Contact List
+// Create the contactsList 2D array
+let contactsList = [
+    ["Poonam W", "poonam.w@gmail.com", "636-555-1234"], // Contact 1
+    ["Ravi N", "ravi.n@yahoo.com", "636-555-5678"] // Contact 2
+];
+console.log('Contacts List:', contactsList);
