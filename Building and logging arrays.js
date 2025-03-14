ac90528ef16a354d9dcd983c73867147c5a14744
PoonamW <poonamwaghode@gmail.com>
2025-03-13 22:59:06 -0500

    Initial commit: Added Building and Logging arrays

---
+++ Building and logging arrays.js
@@ -0,0 +1,19 @@
+// Task 1: Create Arrays
+let groceryItems = ['Apples', 'Bread', 'Milk', 'Eggs', 'Cheese'];
+let prices = [2.99, 1.49, 4.50];
+
+// Task 2: Access and Log the Values
+// 1. Log the entire array of grocery items
+console.log('Grocery Items:', groceryItems);
+
+// 2. Log the entire array of prices
+console.log('Prices:', prices);
+
+// 3. Log three individual items of your choice
+console.log('First item:', groceryItems[0]);
+console.log('Third item:', groceryItems[2]);
+console.log('Fifth item:', groceryItems[4]);
+
+// 4. Log two prices of your choice
+console.log('First price:', prices[0]);
+console.log('Second price:', prices[1]);
